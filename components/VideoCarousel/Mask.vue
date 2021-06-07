c20da76a8ce711e65e75acdde884572cb67e97c5
simarbagga401 <simarbagga401@pop-os.localdomain>
2021-06-07 23:59:49 +0530

    feat(landing page): add typewritter effect

---
+++ components/VideoCarousel/Mask.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <iframe
+      :src="`https://youtube.com/embed/${videoId}`"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      class="video"
+      :class="{ active }"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['videoId', 'active'],
+};
+</script>
+
+<style lang="scss" scoped>
+@use '~/assets/styles/colors';
+
+.video {
+  border: 6px solid colors.$light-black;
+  border-radius: 10px;
+  opacity: 0.5;
+  width: 270px;
+  height: 151.88;
+
+  &.active {
+    opacity: 1;
+    // width: 450px;
+    // height: 253.13px;
+    width: 470px;
+    height: 264.38px;
+  }
+}
+</style>
---
+++ components/VideoCarousel/Track.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="track">
+    <div class="left-btn">
+      <slot name="left-btn" />
+    </div>
+
+    <div class="left">
+      <slot name="left" />
+    </div>
+
+    <div class="center">
+      <slot name="center" />
+    </div>
+
+    <div class="right">
+      <slot name="right" />
+    </div>
+
+    <div class="right-btn">
+      <slot name="right-btn" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.track {
+  display: flex;
+  background: rgb(71, 82, 233);
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+
+  .left {
+    transform: translateX(20px);
+  }
+
+  .right {
+    transform: translateX(-20px);
+  }
+
+  .center {
+    z-index: 10;
+  }
+}
+</style>
